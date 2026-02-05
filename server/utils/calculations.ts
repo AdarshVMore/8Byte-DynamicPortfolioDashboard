2761709ecf60e5fed652acfc3c10912095f3d04b
AdarshVMore <skullcrushermore@gmail.com>
2026-02-06 02:28:42 +0530

    some basic calculations

---
+++ server/utils/calculations.ts
@@ -0,0 +1,15 @@
+export function alculatePresentValue(quantity: number, cmp: number): number {
+  return quantity * cmp;
+}
+
+export function calculateGainLoss(presentValue: number, investment: number): number {
+  return presentValue - investment;
+}
+
+export function calculateGainLossPercent(gainLoss: number, investment: number): number {
+  return (gainLoss / investment) * 100;
+}
+
+export function calculatePortfolioPercent(presentValue: number, totalPresentValue: number): number {
+  return (presentValue / totalPresentValue) * 100;
+}
